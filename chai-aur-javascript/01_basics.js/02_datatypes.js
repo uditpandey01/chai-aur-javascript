61d8a105a09964c3be5af145e8907484be0b4f1d
uditpandey01 <uditpandey8800@gmail.com>
2026-01-08 01:23:56 +0530

    learned about datatypes

--- chai-aur-javascript/01_basics.js/02_datatypes.js
+++ chai-aur-javascript/01_basics.js/02_datatypes.js
@@ -1,1 +1,27 @@
-console.log("practice check-2")
+"use strict"; // treat all js code according to newer version and after this statement we cannot go to previos or old versions
+
+// alert (3 + 3) this statement will return an error as we are using nodejs not browser
+
+// code redability should be high
+
+//  datatypes 
+
+let name = "udit"
+let age = 23
+let isLoggedIn = true
+
+// bigInt => used for greater values 
+// string => preffer double inverted commas
+// boolean => true/false
+// null => it is a datatype but it is a stand alone value used to show nothing
+// undefined => used when we have declared variable but not defined value 
+// symbol => used to find uniqueness
+
+// object
+
+// typeof
+// typeof is used to know the datatype . used as
+console.log(typeof "udit")  // outputs string
+console.log(typeof isLoggedIn) // outputs boolean
+
+
