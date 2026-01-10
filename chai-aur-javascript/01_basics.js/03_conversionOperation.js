10ab621d31b1f25be74e36e420511045c3456bf2
uditpandey01 <uditpandey8800@gmail.com>
2026-01-10 16:59:15 +0530

    operations and conversions

--- chai-aur-javascript/01_basics.js/03_conversionOperation.js
+++ chai-aur-javascript/01_basics.js/03_conversionOperation.js
@@ -1,19 +1,19 @@
 let score = "33";
 
-console.log(typeof score) // to view datatype of score.  here type/datatype is String 
+// console.log(typeof score) // to view datatype of score.  here type/datatype is String 
 // CONVERSION 
 
 let valueInNumber = Number(score)  // we have converted string value in integer and stored it in new variable 
-console.log(typeof valueInNumber) // now type will be number
+// console.log(typeof valueInNumber) // now type will be number
 
 // NOW WE WILL CONVERT ONE ALPHANUMERIC VALUE 
 let one = "33abc"
-console.log(typeof one)
+// console.log(typeof one)
 
 let valueInNumber1 = Number(one)
-console.log(typeof valueInNumber1)   // here our string value gets converted into number 
+// console.log(typeof valueInNumber1)   // here our string value gets converted into number 
 
-console.log(valueInNumber1)  // but here when we print this value it will return NaN i.e Not a Number . Hence our string gets converted into number but that string is not an actual number .
+// console.log(valueInNumber1)  // but here when we print this value it will return NaN i.e Not a Number . Hence our string gets converted into number but that string is not an actual number .
 
 
 // for conversion of NULL it will return 0.
@@ -27,3 +27,37 @@ console.log(valueInNumber1)  // but here when we print this value it will return
 
 
 
+//  ************ OPERATIONS ************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2*2)
+// console.log(2**3) // for raise to power we use double star
+
+
+// string adding
+let str1 = "hello"
+let str2 = " udit"
+let str = str1 + str2
+// console.log(str)
+
+console.log("1" + 2)
+console.log("1" + "2")
+console.log(1 + "2")
+console.log(1 + 2 + "2")     // output : 32
+console.log("1" + 2 + 2)     // output : 122
+
+//  if string is first then all other elements are treated as string and if not then all calculations are done then string is concatenated
+// all these three statements give same output 12
+// these conversions are based on certain guidelines so do not rely on this type of conversions 
+
+let num1 ,num2 ,num3
+num1 = num2 = num3 = 2 + 2
+
+// pre increment or post increment  
