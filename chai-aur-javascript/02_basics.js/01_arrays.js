2aa9d5fe9f10fa20446aa65e4c82ce3a73282d59
uditpandey01 <uditpandey8800@gmail.com>
2026-01-14 18:09:19 +0530

    learned basics of array

---
+++ chai-aur-javascript/02_basics.js/01_arrays.js
@@ -0,0 +1,45 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()   // join function binds and converts all elements of array into a string
+
+// so the type of the output after using join will be string
+
+// console.log(myArr);
+// console.log( newArr);
+
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
+
+//  so here slice function will seperate out that portion given inside the range 
+// where as splice function will remove the portion given inside  the range and outputs the remaining elements inside the array
+// so splice fuction actually manipulates the array 
